71d8349d29f2e7c293f590631efabf9f00b75fb0
Adam Miller <theadammiller56@gmail.com>
2021-06-14 08:17:31 -0500

    Init

---
+++ Text_SourecControl_3/Text_SourecControl_3/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Text_SourecControl_3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
